8c4a48255d7dac216f91e70de0782865344d8e85
daiki-kojima <d.kojima1811@morijyobi.ac.jp>
2020-05-11 16:11:33 +0900

    Androidjs変更

---
+++ templatehtml/JavaScript/Android.js
@@ -0,0 +1,91 @@
+
+/* 数値を入力した際の処理 */
+class Number_Push{
+
+    /* 入力数値読み込み */
+
+    /*　TextBox内値読み込み */
+
+    /* TextBoxがnullの場合の処理 */
+    /* nullの場合の処理ここまで↑ */
+
+    /* TextBox最後尾の値を取得 */
+
+    /* 最後尾が数値の場合の処理 */
+    /* TextBox内の値が0のみか */
+    /* 数値の場合の処理ここまで↑ */
+
+    /* 最後尾が演算子の場合の処理 */
+    /* 負数として扱うか */
+    /* 演算子の場合の処理ここまで↑ */
+
+    /* 最後尾が小数点の場合の処理 */
+    /* 小数点より前に値が存在しない場合0.に修正 */
+    /* 演算子の場合の処理ここまで↑ */
+
+}
+
+/* 演算子を入力した際の処理 */
+class Operator_Push{
+
+    /* 入力値読み込み */
+
+    /*　TextBox内値読み込み */
+
+    /* TextBoxがnullの場合の処理 */
+    /* nullの場合の処理ここまで↑ */
+
+    /* TextBox最後尾の値を取得 */
+
+    /* 最後尾が数値の場合の処理 */
+    /* TextBox内の値が0のみか */
+    /* 数値の場合の処理ここまで↑ */
+
+    /* 最後尾が演算子の場合の処理 */
+    /* 負数として扱うか */
+    /* 演算子の場合の処理ここまで↑ */
+
+    /* 最後尾が小数点の場合の処理 */
+    /* 小数点より前に値が存在しない場合0.に修正 */
+    /* 演算子の場合の処理ここまで↑ */
+}
+
+/* 長押しでClearを行う */
+class Delete_Clear{
+
+}
+
+/* 入力値読み込み */
+function Input_Value_Check(){
+
+}
+
+/* TextBox内値読み込み */
+function TextBox_Check(){
+
+}
+
+/* TextBoxのnullチェック */
+function TextBox_null_Check(){
+
+}
+
+/* 小数点より前に値が存在しない場合0.に修正 */
+function DecimalPoint_Fix(){
+
+}
+
+/* 値が一つしか存在しないかチェック */
+function Number_OneDegit_Check(){
+
+}
+
+/* 入力する値を負数として扱うかチェック */
+function Number_Negative_Check(){
+
+}
+
+/* TextBoxの値が0のみかチェック */
+function TextBox_Zero_Check(){
+
+}
