eb77974ba9721f7b060b2b319e649a70ee2c9dc5
daiki-kojima <d.kojima1811@morijyobi.ac.jp>
2020-05-13 13:22:05 +0900

    Android.js更新

--- templatehtml/JavaScript/Android.js
+++ templatehtml/JavaScript/Android.js
@@ -1,31 +1,73 @@
 
-/* 数値を入力した際の処理 */
-class Number_Push{
+//押下処理
+class Button_Push{
+    constructor(TextBox_Value,TextBox_Last,Result_Value){
+        this.TextBox_Value = TextBox_Value;
+        this.TextBox_Last = TextBox_Last;
+    }
 
-    /* 入力数値読み込み */
+    //TextBox内値読み込み
+    TextBox_Value_Read(){
+        var TextBox_Value;
+        TextBox_Value = document.getElementById("TextBox.Value");
+        return TextBox_Value;
+    }
+    //TextBoxがnullか
+    TextBox_Null_Check(){
+        var Null_TF;
+        if(TextBoxValue_Read() != null){
+            Null_TF = true;
+        }
+        else{
+            Null_TF = false;
+        }
+        return Null_TF;
+    }
+    //TextBox最後尾の値を取得
+    TextBox_Slice(){
+        var TextBox_Last;
+        TextBox_Last = TextBoxValue_Read();
+        TextBox_Last = TextBox_Last.slice(-1);
+        return TextBox_Last;
+    }
+    //最後尾文字の種類判定
+    TextBox_Last_Check(){
+        var Last_Type;
+        if(TextBox_Slice() == "."){
+            Last_type = dec;
+        }else if(TextBox_Slice() == number){
+            Last_Type = Num;
+        }else if(TextBox_Slice() == string){
+            Last_Type = Str;
+        }
+        return Last_Type;
+    }
+    //TextBox内の値が0か
+    TextBox_Zero_Check(){
+        var Zero_TF;
+        if(TextBoxValue_Read() == 0){
+            Zero_TF = true;
+        }
+        else{
+            Zero_TF = false;
+        }
+        return Zero_TF;
+    }
 
-    /*　TextBox内値読み込み */
-
-    /* TextBoxがnullの場合の処理 */
-    /* nullの場合の処理ここまで↑ */
-
-    /* TextBox最後尾の値を取得 */
-
-    /* 最後尾が数値の場合の処理 */
-    /* TextBox内の値が0のみか */
-    /* 数値の場合の処理ここまで↑ */
-
-    /* 最後尾が演算子の場合の処理 */
-    /* 負数として扱うか */
-    /* 演算子の場合の処理ここまで↑ */
-
-    /* 最後尾が小数点の場合の処理 */
-    /* 小数点より前に値が存在しない場合0.に修正 */
-    /* 演算子の場合の処理ここまで↑ */
+    //小数点より前に値が存在しない場合0.に修正
+    DecimalPoint_Fix(){
+        if(TextBox_Value_Read() == "."){
+            TextBox_Value = "0.";
+            return TextBox_Value;
+        }
+    }
 
 }
 
-/* 演算子を入力した際の処理 */
+class Number_Click{
+
+}
+//演算子を入力した際の処理
 class Operator_Push{
 
     /* 入力値読み込み */
@@ -50,25 +92,47 @@ class Operator_Push{
     /* 演算子の場合の処理ここまで↑ */
 }
 
-/* 長押しでClearを行う */
-class Delete_Clear{
+class Calculation extends Button_Push{
+    constructor(TextBox_Value,TextBox_Last,Result_Value){
+        this.TextBox_Value = TextBox_Value;
+        this.TextBox_Last = TextBox_Last;
+        this.Result_Value = Result_Value;
+    }
+    calc(){
+        //TextBox内値読み込み
+        TextBox_Value_Read();
+        Check = TextBox_Null_Check();
+        //TextBoxがnull以外で演算処理移行
+        if(this.Check != true){
+            switch(TextBox_Last_Check()){
+                //TextBox最後尾が数値の場合演算処理
+                case '\d': this.Result_Value = eval(TextBox_Value_Read());
+                return this.Result_Value;
+                break;
+                //TextBox最後尾が小数点の場合
+                case '.' : 
+                break;
+                //TextBox最後尾が文字列の場合
+                case 'string' :
+                break;
+            }
+        }
+    }
+        //TextBox最後尾の値を取得
+    
+        /* 最後尾が数値の場合の処理 */
+    
+    
+        /* 数値の場合の処理ここまで↑ */
 
+        /* 最後尾が演算子の場合の処理 */
+        /* 演算子の場合の処理ここまで↑ */
+
+        /* 最後尾が小数点の場合の処理 */
+        /* 演算子の場合の処理ここまで↑ */
+    
 }
 
-/* 入力値読み込み */
-function Input_Value_Check(){
-
-}
-
-/* TextBox内値読み込み */
-function TextBox_Check(){
-
-}
-
-/* TextBoxのnullチェック */
-function TextBox_null_Check(){
-
-}
 
 /* 小数点より前に値が存在しない場合0.に修正 */
 function DecimalPoint_Fix(){
@@ -87,5 +151,10 @@ function Number_Negative_Check(){
 
 /* TextBoxの値が0のみかチェック */
 function TextBox_Zero_Check(){
+
+}
+
+/* 長押しでClearを行う */
+function Delete_Clear(){
 
 }
